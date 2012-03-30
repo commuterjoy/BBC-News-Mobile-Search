91012bb76e0832abeb4b9b7cb51905d27085fa26
Matt Chadburn <matt@commuterjoy.co.uk>
2012-03-30 09:49:33 +0100

    Removed cruft from controller

--- app/controllers/mobile.rb
+++ app/controllers/mobile.rb
@@ -7,57 +7,17 @@ class Mobile < Sinatra::Base
 # ----------- before
   
   before do
-
-    # does the querystring indicate that we should remove the layout ?
-    @nolayout = (request.query_string =~ /embed/) ? true : false
-    
-    # are we loading secondary content?
-    @secondary = (request.query_string =~ /secondary/) ? true : false
-    
-    # force the layout off when the 'secondary' content is loaded
-    @nolayout = (@secondary) ? false : @nolayout
-
   end
   
 # ----------- after
 
   after do  
-      
-    # caching for Heroku (Varnish) - http://devcenter.heroku.com/articles/http-caching
     headers['Cache-Control'] = 'public, max-age=60'
-
   end
   
 # ----------- helpers
 
   helpers do
-      
-    def agent
-      env['HTTP_USER_AGENT']
-    end
-
-    # stores locales for frontpages - GB, US, PAC etc.
-    def locale
-      env['HTTP_BBC_NEWS_LOCALE'] || nil
-    end
-
-    # ----------- view helpers
-    
-    # open a file in the public folder and return it's contents. like a shit mod_include. # @todo replace with JAMMIT
-    def expand(asset=nil) 
-      data = ''
-      open(File.dirname(__FILE__) + '/../public/' + asset) {|f| data = f.read }
-      data
-    end
-    
-    def image_to_thumb(url=nil)
-      url.gsub!('http://news.bbcimg.co.uk/media/images/', 'http://www.bbc.co.uk/moirastatic/img/iphone/styhalf/')
-    end
-
-    def image_to_ipad(url=nil)
-      url.gsub!('http://news.bbcimg.co.uk/media/images/', 'http://www.bbc.co.uk/moirastatic/img/ipad/styhalf/')
-    end
-      
   end
 
 # ----------- api
@@ -70,7 +30,7 @@ class Mobile < Sinatra::Base
     if @search
        @title = @search['title'].inner_text
     end
-    erb :search, :layout => @nolayout
+    erb :search, :layout => false
   end
   
   # default
@@ -79,6 +39,4 @@ class Mobile < Sinatra::Base
   end
 
 end
-
-
 
